ac931dae0a371d940fb27ff68c209840d64f181b
Jaakko Lukkari <jaakko@unity3d.com>
2017-04-03 01:08:18 +0300

    Move comment related code to own file

---
+++ parser/parser/comment.go
@@ -0,0 +1,42 @@
+package parser
+
+import (
+	"github.com/orktes/orlang/parser/ast"
+	"github.com/orktes/orlang/parser/scanner"
+)
+
+func (p *Parser) processComment(tok scanner.Token) {
+	comment := ast.Comment{Token: tok}
+	if p.commentAfterNodeCheck != nil {
+		node := p.commentAfterNodeCheck
+		if comment.Token.EndLine == node.EndPos().Line {
+			p.nodeComments[node] = append(p.nodeComments[node], comment)
+			return
+		}
+	}
+	p.comments = append(p.comments, comment)
+}
+
+func (p *Parser) checkCommentForNode(node ast.Node, afterNode bool) {
+	if len(p.comments) == 0 {
+		return
+	}
+
+	for i := len(p.comments) - 1; i >= 0; i-- {
+		lastComment := p.comments[i]
+		diff := node.StartPos().Line - lastComment.Token.EndLine
+
+		if diff == 1 || diff == 0 {
+			p.comments = append(p.comments[:i], p.comments[i+1:]...)
+			p.nodeComments[node] = append(p.nodeComments[node], lastComment)
+		} else if diff > 1 {
+			break
+		}
+	}
+
+	if afterNode {
+		p.commentAfterNodeCheck = node
+	}
+
+	return
+}
--- parser/parser/parser.go
+++ parser/parser/parser.go
@@ -119,42 +119,6 @@ loop:
 	return
 }
 
-func (p *Parser) processComment(tok scanner.Token) {
-	comment := ast.Comment{Token: tok}
-	if p.commentAfterNodeCheck != nil {
-		node := p.commentAfterNodeCheck
-		if comment.Token.EndLine == node.EndPos().Line {
-			p.nodeComments[node] = append(p.nodeComments[node], comment)
-			return
-		}
-	}
-	p.comments = append(p.comments, comment)
-}
-
-func (p *Parser) checkCommentForNode(node ast.Node, afterNode bool) {
-	if len(p.comments) == 0 {
-		return
-	}
-
-	for i := len(p.comments) - 1; i >= 0; i-- {
-		lastComment := p.comments[i]
-		diff := node.StartPos().Line - lastComment.Token.EndLine
-
-		if diff == 1 || diff == 0 {
-			p.comments = append(p.comments[:i], p.comments[i+1:]...)
-			p.nodeComments[node] = append(p.nodeComments[node], lastComment)
-		} else if diff > 1 {
-			break
-		}
-	}
-
-	if afterNode {
-		p.commentAfterNodeCheck = node
-	}
-
-	return
-}
-
 func (p *Parser) eof() (ok bool) {
 	if _, ok = p.expectToken(scanner.TokenTypeEOF); !ok {
 		p.unread()
