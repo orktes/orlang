45034daaac106acc80a944a8758e354a587521df
Jaakko Lukkari <jaakko@unity3d.com>
2017-09-09 16:32:03 +0300

    Fix result string

--- codegen/js/jscodegen_test.go
+++ codegen/js/jscodegen_test.go
@@ -193,7 +193,7 @@ func TestSimple(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if res != "result is:  2 and -1 and -25 and 10 and 102 and 0 and 20 and 100 and 4" {
+	if res != "result is: 2 and -1 and -25 and 10 and 102 and 0 and 20 and 100 and 4" {
 		t.Error("Wrong result received", res)
 	}
 }
