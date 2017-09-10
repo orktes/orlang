2629edbecc3c2c6d5cbe7badbb337128120229c5
Jaakko Lukkari <jaakko@unity3d.com>
2017-09-10 15:35:05 +0300

    Revert "Use interface in example"
    This reverts commit e76cb852df85546877a05b823b357753042c9d0b.

--- docs/try/index.js
+++ docs/try/index.js
@@ -26,20 +26,12 @@ struct Point {
   }
 
   fn toString() => string {
-    return "{\n" +
+    return "{\\n" +
            "  x: " + int_to_str(int64(this.x)) +
-           ",\n" +
+           ",\\n" +
            "  y: " + int_to_str(int64(this.y)) +
-           "\n}"
+           "\\n}"
   }
-}
-
-interface Stringer {
-  fn toString() => string
-}
-
-fn toString(str:Stringer) => string {
-  return str.toString()
 }
 
 fn main() {
@@ -48,8 +40,9 @@ fn main() {
 
   var combined = pointA + pointB
 
-  print("Hello world: " + toString(combined))
+  print("Hello world: " + combined.toString())
 }
+
 `;
 
 class App extends React.Component {
